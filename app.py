74bbfbbf4993f77f1081bf008d57d2ea9b85e74d
ArhamZaidi161 <119464413+ArhamZaidi161@users.noreply.github.com>
2025-02-19 21:58:08 -0500

    created stock form and dropdown

--- app.py
+++ app.py
@@ -14,8 +14,18 @@ def inventory():
     cursor.execute("SELECT * FROM inventory")
     inventory_data = cursor.fetchall()
     connection.close()
-    print("Fetched Inventory Data:", inventory_data)  #Debugging Step
     return render_template("inventory.html", inventory=inventory_data) 
+
+@app.route("/update_stock_form")
+def update_stock_form():
+    connection = sqlite3.connect("inventory.db") 
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM inventory")
+    inventory_data = cursor.fetchall()
+    connection.close()
+    return render_template("update_stock_form.html", inventory=inventory_data) 
+
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
